bc217bb20a37e78f539a5a0171de169e1b654c2f
Hieung1232 <hieub2111981@student.ctu.edu.vn>
2025-05-29 17:57:34 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './components/subjectComponent';
-import AddSubject from './pages/addSubject';
-import EditSubject from './pages/editSubject';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
+import Layout      from './components/subjectComponent';
+import AddSubject  from './pages/addSubject';
+import EditSubject from './pages/editSubject';
+import HomePage    from './pages/HomePage';
+import LoginPage   from './pages/LoginPage';
+
 function App() {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Navigate to="/subjects/add" />} />
-        <Route path="/subjects/add" element={<AddSubject />} />
-        <Route path="/subjects/edit/:id" element={<EditSubject />} />
-      </Routes>
-    </Layout>
-  );
     <BrowserRouter>
       <Routes>
-        <Route path="/homepage" element={<HomePage/>} />
-        <Route path="/login" element={<LoginPage/>} />
+        {/* những route không có header/sidebar */}
+        <Route path="/login"   element={<LoginPage />} />
+        <Route path="/homepage" element={<HomePage />} />
+
+        {/* tất cả route còn lại dùng chung Layout */}
+        <Route element={<Layout />}>
+          <Route path="/"                     element={<Navigate to="subjects/add" replace />} />
+          <Route path="subjects/add"          element={<AddSubject />} />
+          <Route path="subjects/edit/:id"     element={<EditSubject />} />
+          {/* nếu có thêm pages khác cũng đặt ở đây */}
+        </Route>
+
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
